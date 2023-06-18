8b2acdf2314ceae91979d8d8ea9e5c66127d4b93
lutifyce <lutifyce@mail.ru>
2023-06-19 00:51:40 +0300

    10.5

--- src/views/Ads/AdListView.vue
+++ src/views/Ads/AdListView.vue
@@ -1,7 +1,7 @@
 <template>
     <v-container>
         <h1 class="text--secondary mb-3 mt-3">My ads</h1>
-        <v-card v-for="ad in ads" :key="ad.id" class="mx-auto" max-width="1000">
+        <v-card v-for="ad in myAds" :key="ad.id" class="mx-auto" max-width="1000">
             <v-row>
                 <v-img :src="ad.src" height="175px"></v-img>
                 <v-spacer></v-spacer>
@@ -22,41 +22,13 @@
         </v-card>
     </v-container>
 </template>
+
 <script>
 export default {
-    data() {
-        return {
-            ads: [
-                {
-                    title: "First",
-                    desc: "First Desc",
-                    promo: true,
-                    src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
-                    id: "1",
-                },
-                {
-                    title: "Second",
-                    desc: "Second Desc",
-                    promo: true,
-                    src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
-                    id: "2",
-                },
-                {
-                    title: "Third",
-                    desc: "Thitd Desc",
-                    promo: true,
-                    src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
-                    id: "3",
-                },
-                {
-                    title: "Fouth",
-                    desc: "Fouth Desc",
-                    promo: true,
-                    src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
-                    id: "4",
-                },
-            ],
-        };
-    },
+    computed: {
+        myAds() {
+            return this.$store.getters.myAds
+        }
+    }
 };
 </script>
