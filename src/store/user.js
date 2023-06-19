bd2a02b239c8e2814c3bc419cd8a5fe5fa24c9c1
lutifyce <lutifyce@mail.ru>
2023-06-19 04:15:22 +0300

    15.4

--- src/store/user.js
+++ src/store/user.js
@@ -78,6 +78,9 @@ export default {
   getters: {
       user(state) {
           return state.user
-      }
+      },
+      isUserLoggedIn (state) {
+          return state.user !== null
+      }        
   }
 }
